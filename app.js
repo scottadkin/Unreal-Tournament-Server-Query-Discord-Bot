65b1f9f81318c92123c8f4ca43797d62efe84f15
Scott Adkin <scottadkin001@gmail.com>
2026-05-03 13:42:16 +0100

    update version, removed comment

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
 const Bot = require('./api/bot.js');
 
 new Bot();
-
-
-/*setInterval(() =>{
-
-    //console.log(process.memoryUsage());
-
-    const toMb = (input) =>{
-
-        return `${(input / 1024 / 1024).toFixed(2)}MB`;
-    }
-
-    const m = process.memoryUsage();
-
-    console.clear();
-    console.log(`RSS ${toMb(m.rss)} heapTotal ${toMb(m.heapTotal)} heapUsed ${toMb(m.heapUsed)} external ${toMb(m.external)}`);
-
-}, 1000);*/
